78289e6d29dd45e1baacb762e0a349a0891878a6
Daniel Däschle <daniel.daeschle@gmail.com>
2020-11-17 15:30:43 +0100

    tests: update tests

--- vjwt_test.v
+++ vjwt_test.v
@@ -2,7 +2,20 @@ module main
 
 import jwt
 
-fn test_encode_map() {
+fn test_encode_map_hs256() {
+	payload := {
+		'sub': '1234567890'
+		'name': 'John Doe'
+		'iat': '1516239022'
+	}
+	token := jwt.encode({
+		payload: payload
+		key: 'secret'
+	})
+	assert token == 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoiMTUxNjIzOTAyMiJ9.1pGITVN1_HkzPxvMfRvTNEmJSiCSUZ_YVWhRt4AmYYw'
+}
+
+fn test_encode_map_hs512() {
 	payload := {
 		'sub': '1234567890'
 		'name': 'John Doe'
